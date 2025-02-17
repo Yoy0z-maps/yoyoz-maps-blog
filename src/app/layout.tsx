1d5e23a0fb3482bda1c5dfcfdcf9331d30f45670
John Han <work.johnhan@gmail.com>
2025-02-17 08:43:21 +0800

    Fix: Build Error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import Footer from "@/components/Footer";
 import localFont from "next/font/local";
 
 const pretendard = localFont({
-  src: "../../public/fonts/PretendardVariable.woff2",
+  src: "../../public/fonts/pretendard-variable.woff2",
   display: "swap",
   weight: "45 920",
   variable: "--font-pretendard",
