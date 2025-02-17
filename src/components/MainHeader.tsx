49046d621e8c3017768e4b13f90a8d228f4ea674
John Han <work.johnhan@gmail.com>
2025-02-17 09:21:55 +0800

    Feat/ Web Theme

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -10,9 +10,11 @@ const navigationItems = [
 
 export default function MainHeader() {
   return (
-    <div className="fixed top-0 w-full flex items-center justify-between py-6 px-9 z-50">
+    <div className="fixed top-0 w-full flex items-center justify-between py-4 px-9 z-50">
       <div id="logo" className="flex flex-shrink-0 items-center">
-        <p className="font-pretendard text-white text-2xl">Yoy0z-maps.</p>
+        <p className="font-pretendard text-logo-light dark:text-white text-xl">
+          Yoy0z-maps.
+        </p>
       </div>
       <nav className="flex items-center justify-center gap-x-7">
         {navigationItems.map((item) => (
