2e7425678585bf260f9f4e8fa0c1037958f201d7
John Han <work.johnhan@gmail.com>
2025-03-25 16:38:41 +0800

    API 주소 등록

--- src/components/admin/login/AdminLoginLogo.tsx
+++ src/components/admin/login/AdminLoginLogo.tsx
@@ -1,9 +1,10 @@
+"use client";
 import { MdAdminPanelSettings } from "react-icons/md";
 
 export default function AdminLoginLogo() {
   return (
-    <div className="flex flex-col items-center gap-4 mb-6">
-      <MdAdminPanelSettings size={50} className="text-blue-600" />
+    <div className="flex flex-col items-center gap-4 mb-6 ">
+      <MdAdminPanelSettings className="w-12 h-12 text-blue-600" />
       <p className="text-2xl font-semibold font-pretendard text-blue-600">
         Admin Login
       </p>
---
+++ src/constant/api_address.ts
@@ -0,0 +1,1 @@
+export const API_SERVER_ADDRESS = process.env.API_SERVER_ADDRESS;
