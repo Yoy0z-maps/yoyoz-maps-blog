65508e359e3884114507fc717cd2fcbf29186ffc
John Han <work.johnhan@gmail.com>
2025-04-05 16:06:34 +0800

    Docs: projects.ts add content

--- src/constant/projects.ts
+++ src/constant/projects.ts
@@ -21,32 +21,42 @@ export const projects = [
   },
   {
     id: "03",
+    title: "Yoy0z-maps Blog",
+    description:
+      "I created a personal web service where I introduce myself and reflect on the things I’ve learned and felt while working on both individual and team projects. ",
+    image: "/assets/images/project/test6.png",
+    link: "",
+    skills: ["Next.js", "AWS", "Typescript", "Django", "Vercel"],
+    delay: 0.5,
+  },
+  {
+    id: "04",
     title: "JOYURI OFFICIAL",
     description:
       "You can explore various information about the celebrity Jo Yuri, including her appearances, schedules, albums, and more.",
     image: "/assets/images/project/test3.png",
     link: "",
     skills: ["Next.js", "Django", "Typescript", "AWS", "Figma"],
-    delay: 0.5,
+    delay: 0,
   },
   {
-    id: "04",
+    id: "05",
     title: "My Trip Tracker",
     description:
       "My Trip Tracker was inspired by the portfolio of Jongmin Kim. It captures the memories of the people I’ve traveled with.",
     image: "/assets/images/project/test4.png",
     link: "",
     skills: ["Next.js", "Django", "Typescript", "AWS", "Figma"],
-    delay: 0,
+    delay: 0.25,
   },
   {
-    id: "05",
+    id: "06",
     title: "Obunga Discord Bot",
     description:
       "This is the simplest but the first project I made when I started coding. It allows music playback and mini-games within a Discord server, and it provides game information requested by users through web crawling.",
     image: "/assets/images/project/test5.jpeg",
     link: "",
     skills: ["Python", "Discord API", "AWS"],
-    delay: 0.25,
+    delay: 0.5,
   },
 ];
