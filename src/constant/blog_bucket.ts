96efb7a5b036e3f33655289af12798df8a43ae40
John Han <work.johnhan@gmail.com>
2025-04-23 12:55:34 +0800

    blog_bucket 글 추가

--- src/constant/blog_bucket.ts
+++ src/constant/blog_bucket.ts
@@ -5,18 +5,22 @@ export const BLOG_BUCKET = [
   },
   {
     id: 1,
-    target: "0e6c2d9d-48e2-4b63-b4a7-58089a62b72b",
+    target: "1a56de6d-63c5-4251-ae7f-a64f882661f1",
   },
   {
     id: 2,
-    target: "69face6a-abf4-45d6-83f1-c8e649c9c328",
+    target: "0e6c2d9d-48e2-4b63-b4a7-58089a62b72b",
   },
   {
     id: 3,
-    target: "79f44627-fb84-4d4f-aed8-ccdbc52b8704",
+    target: "69face6a-abf4-45d6-83f1-c8e649c9c328",
   },
   {
     id: 4,
+    target: "79f44627-fb84-4d4f-aed8-ccdbc52b8704",
+  },
+  {
+    id: 5,
     target: "6f751ca3-6007-4e7d-9f32-39b999d270b3",
   },
 ];
