c03896e12c9190d86db82800dfd0d64df997513a
John Han <work.johnhan@gmail.com>
2025-09-08 13:59:47 +0800

    FIX: Blog=>Category Value

--- src/constant/categories.ts
+++ src/constant/categories.ts
@@ -2,5 +2,5 @@ export const CATEGORIES = [
   { text: "All", id: 0, value: "ALL" },
   { text: "Dev", id: 1, value: "DEV" },
   { text: "Design", id: 2, value: "DESIGN" },
-  { text: "Life", id: 3, value: "LIFESTYLE" },
+  { text: "Life", id: 3, value: "LIFE" },
 ];
